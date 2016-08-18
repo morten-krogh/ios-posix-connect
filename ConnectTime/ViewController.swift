3f575bdfd51dfbb199390acc2fbdadd490fb0ac4
Morten Krogh <morten.krogh@amberbio.com>
2016-08-18 11:57:23 +0200

    repeated connects

--- ConnectTime/ViewController.swift
+++ ConnectTime/ViewController.swift
@@ -28,9 +28,32 @@ class ViewController: UIViewController {
         button.setTitle("Tap to connect to server", forState: .Normal)
         button.setTitleColor(UIColor.blueColor(), forState: .Normal)
         button.frame = CGRectMake(15, 250, 400, 100)
-        button.addTarget(self, action: #selector(self.buttonClicked), forControlEvents: .TouchUpInside)
+        button.addTarget(self, action: #selector(self.repeated_connect), forControlEvents: .TouchUpInside)
         self.view.addSubview(label)
         self.view.addSubview(button)
+
+//        repeated_connect();
+
+    }
+
+    func repeated_connect() {
+        let start = NSDate()
+        let rc = connect_to_server();
+        let end = NSDate()
+        let duration: Double = end.timeIntervalSinceDate(start)
+
+        var result = ""
+        if (rc == 0) {
+            result = "Duration: \(duration)"
+        } else {
+            result = "error"
+        }
+        label.text = result
+
+        if (rc == 0) {
+            let delay = 1 as Double // time in seconds
+            NSTimer.scheduledTimerWithTimeInterval(delay, target: self, selector: #selector(repeated_connect), userInfo: nil, repeats: false)
+        }
     }
 
     override func didReceiveMemoryWarning() {
