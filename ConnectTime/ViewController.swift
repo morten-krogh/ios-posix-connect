ed5029e45ccf1d5f6f475005d92ebbddb78552f2
Adam Fish <af@realm.io>
2016-09-20 16:58:25 -0700

    Updated the project to be compatible with Swift 3 and Xcode 8.
    Also, added a test to use Core Foundation level APIs instead of POSIX.
    
    To toggle, adjust the useFoundation variable to true or false.
    
    With iPad Pros, the POSIX issue of not activating the antenna is observable. I can register 3-4 second delays, but when using Core Foundation APIs the delay is typically 0.4 seconds, and never beyond 1 sec in my testing.

--- ConnectTime/ViewController.swift
+++ ConnectTime/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let label = UILabel()
+    let useFoundation = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,15 +21,15 @@ class ViewController: UIViewController {
         let button = UIButton()
 
         label.text = "Result"
-        label.textColor = UIColor.redColor()
-        label.textAlignment = .Center
+        label.textColor = UIColor.red
+        label.textAlignment = .center
         label.numberOfLines = 1
-        label.frame = CGRectMake(15, 50, 400, 100)
+        label.frame = CGRect(x: 15, y: 50, width: 400, height: 100)
 
-        button.setTitle("Tap to connect to server", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        button.frame = CGRectMake(15, 250, 400, 100)
-        button.addTarget(self, action: #selector(self.buttonClicked), forControlEvents: .TouchUpInside)
+        button.setTitle("Tap to connect to server", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.frame = CGRect(x: 15, y: 250, width: 400, height: 100)
+        button.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
         self.view.addSubview(label)
         self.view.addSubview(button)
     }
@@ -41,23 +42,95 @@ class ViewController: UIViewController {
     func buttonClicked(sender: UIButton!) {
 
         let start = NSDate()
-
-        let rc = connect_to_server();
-
+        
+        var success = false
+        
+        if useFoundation {
+            if connectWithCFStream() {
+                success = true
+            }
+        }
+        else {
+            let rc = connect_to_server()
+            
+            if (rc == 0) {
+                success = true
+            }
+        }
+        
         let end = NSDate()
-
-        let duration: Double = end.timeIntervalSinceDate(start)
-
+        
+        let duration: Double = end.timeIntervalSince(start as Date)
+        
         var result = ""
-
-        if (rc == 0) {
+        
+        if success == true {
             result = "Duration: \(duration)"
-        } else {
+        }
+        else {
             result = "error"
         }
 
         label.text = result
     }
-
+    
+    func connectWithCFStream() -> Bool {
+        var readStream : Unmanaged<CFReadStream>?
+        var writeStream : Unmanaged<CFWriteStream>?
+        let host : CFString = NSString(string: "212.116.89.62")
+        let port : UInt32 = UInt32(80)
+        
+        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, host, port, &readStream, &writeStream)
+        
+        let inputStream: InputStream = readStream!.takeUnretainedValue()
+        let outputStream: OutputStream = writeStream!.takeUnretainedValue()
+        
+        inputStream.schedule(in: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
+        outputStream.schedule(in: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
+        
+        inputStream.open()
+        outputStream.open()
+        
+        while(!outputStream.hasSpaceAvailable) {
+            write(stream: outputStream)
+            close(stream: outputStream)
+            break
+        }
+        
+        var result = ""
+        
+        while (!inputStream.hasBytesAvailable) {
+            result = read(stream: inputStream)
+            close(stream: inputStream)
+            break
+        }
+        
+        let index = result.index(result.startIndex, offsetBy: 12)
+        
+        if result.substring(to: index) == "HTTP/1.1 400" {
+            return true
+        }
+        
+        return false
+    }
+    
+    func write(stream: OutputStream) {
+        let getRequest = "GET http://www.amberbio.com/connect_time HTTP/1.1\r\n\r\n".data(using: String.Encoding.utf8)!
+        
+        _ = getRequest.withUnsafeBytes { stream.write($0, maxLength: getRequest.count) }
+    }
+    
+    func read(stream: InputStream) -> String {
+        var buffer = [UInt8](repeating: 0, count: 1000)
+        let _ = stream.read(&buffer, maxLength: buffer.count)
+        let output = NSString(bytes: &buffer, length: buffer.count, encoding: String.Encoding.utf8.rawValue)
+        
+        return output as! String
+    }
+    
+    func close(stream: Stream) {
+        stream.close()
+        stream.remove(from: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
+    }
 }
 
